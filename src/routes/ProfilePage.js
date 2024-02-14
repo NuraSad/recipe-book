8373a50f796c762f17f1dbb5b6ce246e2a8ef3a8
Nura <ainur.saduova10@gmail.com>
2024-02-14 17:07:44 -0500

    start style user profile

--- src/routes/ProfilePage.js
+++ src/routes/ProfilePage.js
@@ -12,42 +12,48 @@ export default function Profile() {
   const [username] = useOutletContext();
 
   return (
-    <div id="profile field">
-      <h1>{username}</h1>
-      {created.length ? (
-        <ul>
+    <div className="profile-field">
+      <h1>{username}'s profile</h1>
+      <div className="table-field">
+        <div className="profile-column">
           <span>Your created recipes:</span>
-          {created.map((recipe) => (
-            <li key={recipe._id}>
-              {/* Link to without goes to the relative path, whereas with / find route relative to root path /  */}
-              <Link to={`/recipes/${recipe._id}`}>
-                {recipe.name ? <>{recipe.name}</> : <i>No Name</i>}{" "}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>
-          <i>No recipes</i>
-        </p>
-      )}
-      {favourite.length ? (
-        <ul>
-          <span>Your liked recipes:</span>
-          {favourite.map((recipe) => (
-            <li key={recipe._id}>
-              {/* Link to without goes to the relative path, whereas with / find route relative to root path /  */}
-              <Link to={`/recipes/${recipe._id}`}>
-                {recipe.name ? <>{recipe.name}</> : <i>No Name</i>}{" "}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>
-          <i>No recipes</i>
-        </p>
-      )}
+          {created.length ? (
+            <ul>
+              {created.map((recipe) => (
+                <li key={recipe._id}>
+                  {/* Link to without goes to the relative path, whereas with / find route relative to root path /  */}
+                  <Link to={`/recipes/${recipe._id}`}>
+                    {recipe.name ? <>{recipe.name}</> : <i>No Name</i>}{" "}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>You didn't create any recipes =( </i>
+            </p>
+          )}
+        </div>
+        <div className="profile-column">
+          <span>Your favorite recipes:</span>
+          {favourite.length ? (
+            <ul>
+              {favourite.map((recipe) => (
+                <li key={recipe._id}>
+                  {/* Link to without goes to the relative path, whereas with / find route relative to root path /  */}
+                  <Link to={`/recipes/${recipe._id}`}>
+                    {recipe.name ? <>{recipe.name}</> : <i>No Name</i>}{" "}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>You didn't like any recipes.</i>
+            </p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
